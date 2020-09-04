e8e7a6c7812423bc916ab84d783ed360f1a7d26f
Alexander von Weiss <alexvonweiss@googlemail.com>
2020-09-04 11:40:03 +0200

    add support for file.shouldWatch

--- cypress/plugins/esbuild-preprocessor.js
+++ cypress/plugins/esbuild-preprocessor.js
@@ -1,6 +1,8 @@
 const {build} = require('esbuild');
 const path = require('path');
+const fs = require('fs');
 const bundles = {};
+const watching = new Set();
 
 module.exports.esbuildPreprocessor = function (options) {
     options = options || {};
@@ -14,6 +16,13 @@ module.exports.esbuildPreprocessor = function (options) {
 
         const outfile = path.extname(file.outputPath) === '.js' ? file.outputPath : `${file.outputPath}.js`;
         const entryPoints = [filePath].concat(options.additionalEntries || []);
+
+        if (file.shouldWatch) {
+            watch(filePath, {
+                onInit: (watcher) => file.on('close', () => watcher.close()),
+                onChange: () => file.emit('rerun'),
+            });
+        }
 
         return (bundles[filePath] = build({
             entryPoints,
@@ -33,3 +42,28 @@ module.exports.esbuildPreprocessor = function (options) {
             }));
     };
 };
+
+function watch(file, callback) {
+    if (watching.has(file)) {
+        return;
+    }
+
+    const emitter = fs.watch(file, {encoding: null}, (event) => {
+        if (event === 'change') {
+            callback.onChange();
+        }
+    });
+
+    callback.onInit(emitter);
+
+    watching.add(file);
+
+    emitter.on('close', () => {
+        watching.delete(file);
+    });
+
+    emitter.on('error', (error) => {
+        console.error(error);
+        watching.delete(file);
+    });
+}
